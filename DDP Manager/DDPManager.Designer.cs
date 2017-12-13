47cca1d45cb0247c8b38fe0ad9e6bb9f079405f9
Snowy <snewygit@gmail.com>
2017-12-13 14:32:53 +0100

    * Initial commit.

---
+++ DDP Manager/DDPManager.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace DDP_Manager
+{
+    partial class DPPManager
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DPPManager));
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.cbURL = new System.Windows.Forms.ComboBox();
+            this.lbContent = new System.Windows.Forms.ListBox();
+            this.cbReentry = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(93, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // cbURL
+            // 
+            this.cbURL.AllowDrop = true;
+            this.cbURL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbURL.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbURL.FormattingEnabled = true;
+            this.cbURL.Items.AddRange(new object[] {
+            "http://www.deutsche-dj-playlist.de/DDP-Charts/",
+            "http://www.deutsche-dj-playlist.de/DDP-Charts-Top100/",
+            "http://www.deutsche-dj-playlist.de/DDP-Charts-Hot50/",
+            "http://www.deutsche-dj-playlist.de/DDP-Charts-Neueinsteiger/",
+            "http://www.deutsche-dj-playlist.de/DDP-Charts-Regional/?acn=7",
+            "http://www.deutsche-dj-playlist.de/DDP-Jahrescharts/?ddpJahr=ewig",
+            "http://www.deutsche-dj-playlist.de/DDP-Jahrescharts/?ddpJahr=2005",
+            "http://www.deutsche-dj-playlist.de/DDP-Jahrescharts/?ddpJahr=2006",
+            "http://www.deutsche-dj-playlist.de/DDP-Jahrescharts/?ddpJahr=2007",
+            "http://www.deutsche-dj-playlist.de/DDP-Jahrescharts/?ddpJahr=2008",
+            "http://www.deutsche-dj-playlist.de/DDP-Jahrescharts/?ddpJahr=2009",
+            "http://www.deutsche-dj-playlist.de/DDP-Jahrescharts/?ddpJahr=2010",
+            "http://www.deutsche-dj-playlist.de/DDP-Jahrescharts/?ddpJahr=2011",
+            "http://www.deutsche-dj-playlist.de/DDP-Jahrescharts/?ddpJahr=2012",
+            "http://www.deutsche-dj-playlist.de/DDP-Jahrescharts/?ddpJahr=2013",
+            "http://www.deutsche-dj-playlist.de/DDP-Jahrescharts/?ddpJahr=2014",
+            "http://www.deutsche-dj-playlist.de/DDP-Jahrescharts/?ddpJahr=2015",
+            "http://www.deutsche-dj-playlist.de/DDP-Jahrescharts/?ddpJahr=2016",
+            "http://www.deutsche-dj-playlist.de/DDP-Schlager-Charts/",
+            "http://www.deutsche-dj-playlist.de/DDP-Schlager-Charts-Top100/",
+            "http://www.deutsche-dj-playlist.de/DDP-Schlager-Charts-Hot50/",
+            "http://www.deutsche-dj-playlist.de/DDP-Schlager-Charts-Neueinsteiger/"});
+            this.cbURL.Location = new System.Drawing.Point(174, 12);
+            this.cbURL.Name = "cbURL";
+            this.cbURL.Size = new System.Drawing.Size(465, 21);
+            this.cbURL.TabIndex = 5;
+            // 
+            // lbContent
+            // 
+            this.lbContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbContent.FormattingEnabled = true;
+            this.lbContent.Location = new System.Drawing.Point(12, 41);
+            this.lbContent.Name = "lbContent";
+            this.lbContent.Size = new System.Drawing.Size(699, 368);
+            this.lbContent.TabIndex = 7;
+            this.lbContent.DoubleClick += new System.EventHandler(this.lbContent_DoubleClick);
+            // 
+            // cbReentry
+            // 
+            this.cbReentry.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbReentry.AutoSize = true;
+            this.cbReentry.Location = new System.Drawing.Point(645, 16);
+            this.cbReentry.Name = "cbReentry";
+            this.cbReentry.Size = new System.Drawing.Size(66, 17);
+            this.cbReentry.TabIndex = 8;
+            this.cbReentry.Text = "Re-entry";
+            this.cbReentry.UseVisualStyleBackColor = true;
+            // 
+            // DPPManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 426);
+            this.Controls.Add(this.cbReentry);
+            this.Controls.Add(this.lbContent);
+            this.Controls.Add(this.cbURL);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DPPManager";
+            this.Text = "DDP Manager";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ComboBox cbURL;
+        private System.Windows.Forms.ListBox lbContent;
+        private System.Windows.Forms.CheckBox cbReentry;
+    }
+}
+
---
+++ DDP Manager/DDPManager.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Security.AccessControl;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace DDP_Manager
+{
+    public partial class DPPManager : Form
+    {
+
+        public DPPManager()
+        {
+            InitializeComponent();
+            this.cbURL.SelectedIndex = 0;
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            lbContent.Items.Clear();
+
+            try
+            {
+                WebRequest request = WebRequest.Create(cbURL.Text);
+                request.Method = "GET";
+                WebResponse response = request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                string content = reader.ReadToEnd();
+                reader.Close();
+                response.Close();
+                
+                int pFrom = 0, pTo = 0, rank = 0;
+                String result = String.Empty, artist = String.Empty, track = String.Empty, title = String.Empty;
+
+                pFrom = content.IndexOf("<h1>") + "<h1>".Length;
+                pTo = content.IndexOf("</h1>", pFrom);
+                title = content.Substring(pFrom, pTo - pFrom);
+                this.Text = title;
+
+                content = content.Remove(0, content.IndexOf("<div class=\"eintrag\">") + "<div class=\"eintrag\">".Length);
+                for(var iterator = 0; iterator < 100; ++iterator)
+                {
+                    pFrom = content.IndexOf("<div class=\"platz\">") + "<div class=\"platz\">".Length;
+                    pTo = content.IndexOf("</div>", pFrom);
+                    String strRank = content.Substring(pFrom, pTo - pFrom);
+                    rank = Int32.Parse(content.Substring(pFrom, pTo - pFrom));
+                    content = content.Remove(0, content.IndexOf("<div class=\"platz\">") + "<div class=\"platz\">".Length);
+
+                    pFrom = content.IndexOf("<div class=\"platz\">") + "<div class=\"platz\">".Length;
+                    pTo = content.IndexOf("</div>", pFrom);
+                    String res = content.Substring(pFrom, pTo - pFrom);
+                    bool isReentry = (res == "RE");
+                    content = content.Remove(0, content.IndexOf("<div class=\"info "));
+
+                    pFrom = content.IndexOf("<div class=\"info ") + "<div class=\"info ".Length;
+                    pTo = content.IndexOf("<div class=\"icons\">");
+                    result = content.Substring(pFrom, pTo - pFrom);
+
+                    pFrom = result.IndexOf("<div class=\"interpret\">") + "<div class=\"interpret\">".Length;
+                    pTo = result.IndexOf("</div>");
+                    artist = result.Substring(pFrom, pTo - pFrom);
+
+                    result = result.Remove(0, pTo + "</div>".Length);
+                    pFrom = result.IndexOf("<div class=\"titel\">") + "<div class=\"titel\">".Length;
+                    pTo = result.IndexOf("</div>");
+                    track = result.Substring(pFrom, pTo - pFrom);
+
+                    result = rank.ToString().PadLeft(3, '0') + " - " + artist + " - " + track + "\n";
+                    result = result.ToLower();
+                    result = Regex.Replace(result, @"(^\w)|(\s\w)", m => m.Value.ToUpper());
+                    result = Regex.Replace(result, @"\({1}[a-z]", m => m.Value.ToUpper());
+
+                    if (!cbReentry.Checked || (cbReentry.Checked && isReentry))
+                    {
+                        lbContent.Items.Add(result);
+                    }
+
+                    content = content.Remove(0, content.IndexOf("<div class=\"icons\">") + "<div class=\"icons\">".Length);
+                }
+            }
+            catch(Exception)
+            {
+                if (lbContent.Items.Count == 0 && !cbReentry.Checked)
+                {
+                    lbContent.Items.Add("Failed to load the page content.");
+                }
+                else if (lbContent.Items.Count == 0 && cbReentry.Checked)
+                {
+                    lbContent.Items.Add("There are no re-entries.");
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sF1 = new SaveFileDialog();
+            sF1.Filter = "Text Files|*.txt";
+            if (sF1.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(sF1.FileName);
+                List<string> list = new List<string>();
+
+                foreach (var item in lbContent.Items)
+                {
+                    string str = item.ToString();
+                    str = Regex.Replace(str, @"\r\n?|\n", String.Empty);
+                    list.Add(str);
+                }
+                string res = String.Join("\r\n", list);
+
+                sw.Write(res);
+
+                sw.Close();
+            }
+        }
+
+        private void lbContent_DoubleClick(object sender, EventArgs e)
+        {
+            string content = lbContent.SelectedItem.ToString();
+            var splitted = content.Split('-');
+            string artist = splitted[1].Remove(0, 1);
+            artist = artist.Replace("Feat. ", "");
+            string title = splitted[2].Remove(0, 1);
+            title = title.Remove(title.Length - 1);
+            Clipboard.SetText(artist + title);
+        }
+    }
+}
